7399f364c5512f37319fe6defd4696b4faffe92c
Alx_Bx <cristalexander@gmail.com>
2020-11-10 15:45:04 -0600

    add parser

--- parser/search.rb
+++ parser/search.rb
@@ -4,4 +4,15 @@ products: html.css('#product-listing-container .ss-results')  #titulo ->  .card 
 products.each do |product|
     name = product.css('.card .card-title').text.strip
     price = product.css('.card-text .ng-scope').text.strip
+    url = product.css('.ng-binding').attr('ng-href')
+        page = {
+            url: url,
+            page_type: product,
+            vars: {
+                name: name, 
+                price: price
+            }
+        }
+
+        pages << page
 end
