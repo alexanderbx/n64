ffba7094a309b003457d73d1ab07a935a54fc970
Alx_Bx <cristalexander@gmail.com>
2020-11-10 12:46:45 -0600

    create seeder

---
+++ seeder/seeder.rb
@@ -0,0 +1,4 @@
+pages << {
+    url: 'https://www.dkoldies.com/n64-games/?utm_source=Bing&utm_medium=cpc&utm_campaign=Bing&msclkid=c3f420b006b315f063c27f1ade380d6f&utm_term=nintendo%2064&utm_content=Games-N64%2FGms-v01',
+    page_type: search
+}
