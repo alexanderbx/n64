345e53852f442efb9114762f6534564185035bf2
Alx_Bx <cristalexander@gmail.com>
2020-11-12 16:03:45 -0600

    add parser-product

--- parsers/product.rb
+++ parsers/product.rb
@@ -1,9 +1,3 @@
 html: Nokogiri.HTML (content)
 condition = html.css('.productView-info-value').text.strip
 
-outputs << {
-    _collection: 'products',
-    status = 'condition',
-    name: page['vars']['name'],
-    price: page['vars']['price']
-}
